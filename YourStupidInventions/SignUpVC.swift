84f2d92b365490fa4a8d089d9bbb38073510859b
IsseiMori <ganntyu1225issei@gmail.com>
2018-08-26 13:08:46 -0700

    add SignUpVC UI

---
+++ YourStupidInventions/SignUpVC.swift
@@ -0,0 +1,190 @@
+//
+//  SignUpVC.swift
+//  YourStupidInventions
+//
+//  Created by MoriIssei on 8/26/18.
+//  Copyright © 2018 IsseiMori. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    // profile image
+    @IBOutlet weak var avaImg: UIImageView!
+    
+    // text fields
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var repeatPasswordTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var fullnameTxt: UITextField!
+    @IBOutlet weak var bioTxt: UITextField!
+    
+    // buttons
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var cancelBtn: UIButton!
+    
+    // scroll view
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    // reset default height
+    var scrollViewHeight: CGFloat = 0
+    
+    // keyboard frame size
+    var keyboard = CGRect()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // scrollView frame size
+        scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        scrollView.contentSize.height = self.view.frame.size.height
+        scrollViewHeight = scrollView.frame.size.height
+        
+        // check notifications if keyboard is shown or not
+        NotificationCenter.default.addObserver(self, selector: #selector(self.showKeyboard), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.hideKeyboard), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        // declare hide keyboard tap
+        let hideTap = UITapGestureRecognizer(target: self, action: #selector(self.hideKeyboardTap))
+        hideTap.numberOfTapsRequired = 1
+        self.view.isUserInteractionEnabled = true
+        self.view.addGestureRecognizer(hideTap)
+        
+        // round ava
+        avaImg.layer.cornerRadius = avaImg.frame.size.width / 2
+        avaImg.clipsToBounds = true
+        
+        // declare select image tap
+        let avaTap = UITapGestureRecognizer(target: self, action: #selector(self.loadImg))
+        avaTap.numberOfTapsRequired = 1
+        avaImg.isUserInteractionEnabled = true
+        avaImg.addGestureRecognizer(avaTap)
+        
+        
+        // alignment
+        avaImg.frame = CGRect(x: self.view.frame.size.width / 2 - 40, y: 40, width: 80, height: 80)
+        usernameTxt.frame = CGRect(x: 10, y: avaImg.frame.origin.y + 90, width: self.view.frame.width - 20, height: 30)
+        passwordTxt.frame = CGRect(x: 10, y: usernameTxt.frame.origin.y + 40, width: self.view.frame.width - 20, height: 30)
+        repeatPasswordTxt.frame = CGRect(x: 10, y: passwordTxt.frame.origin.y + 40, width: self.view.frame.width - 20, height: 30)
+        
+        emailTxt.frame = CGRect(x: 10, y: repeatPasswordTxt.frame.origin.y + 60, width: self.view.frame.width - 20, height: 30)
+        fullnameTxt.frame = CGRect(x: 10, y: emailTxt.frame.origin.y + 40, width: self.view.frame.width - 20, height: 30)
+        bioTxt.frame = CGRect(x: 10, y: fullnameTxt.frame.origin.y + 40, width: self.view.frame.width - 20, height: 30)
+        
+        signUpBtn.frame = CGRect(x: 10, y: bioTxt.frame.origin.y + 50, width: self.view.frame.size.width / 4, height: 30)
+        signUpBtn.layer.cornerRadius = signUpBtn.frame.size.width / 20
+        
+        cancelBtn.frame = CGRect(x: self.view.frame.size.width - self.view.frame.size.width / 4 - 20, y: signUpBtn.frame.origin.y, width: self.view.frame.size.width / 4, height: 30)
+        cancelBtn.layer.cornerRadius = cancelBtn.frame.size.width / 20
+        
+        // background
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        bg.image = UIImage(named: "bg.jpg")
+        bg.layer.zPosition = -1
+        self.view.addSubview(bg)
+        
+    }
+    
+    
+    // call picker to select image
+    @objc func loadImg(recognizer: UITapGestureRecognizer) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    
+    // connect selected image to imageView
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        avaImg.image = info[UIImagePickerControllerEditedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    // hide keyboard if tapped
+    @objc func hideKeyboardTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
+    // show keyboard func
+    @objc func showKeyboard(notification: NSNotification) {
+        
+        // define keyboard size
+        keyboard = ((notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue)!
+        
+        // move up UI
+        UIView.animate(withDuration: 0.4) {
+            self.scrollView.frame.size.height = self.scrollViewHeight - self.keyboard.height
+        }
+        
+        
+    }
+    
+    
+    // hide keyboard func
+    @objc func hideKeyboard(notification: NSNotification) {
+        // move down UI
+        UIView.animate(withDuration: 0.4) {
+            self.scrollView.frame.size.height = self.view.frame.height
+        }
+    }
+    
+    
+    // clicked sing up
+    @IBAction func singUpBtn_clicked(_ sender: Any) {
+        
+        // dismiss keyboard
+        self.view.endEditing(true)
+        
+        // if fields are empty
+        if usernameTxt.text!.isEmpty || passwordTxt.text!.isEmpty || repeatPasswordTxt.text!.isEmpty || emailTxt.text!.isEmpty || fullnameTxt.text!.isEmpty || bioTxt.text!.isEmpty {
+            
+            // alert message
+            alert(title: "Please", message: "fill all fields")
+            
+            return
+        }
+        
+        // if different password
+        if passwordTxt.text != repeatPasswordTxt.text {
+            
+            // alert message
+            alert(title: "Error", message: "passwords do not match")
+            
+            return
+        }
+        
+        
+    }
+    
+    
+    // alert func
+    func alert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        let ok = UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    // clicked cancel
+    @IBAction func cancelBtn_clicked(_ sender: Any) {
+        
+        // hide keyboard
+        self.view.endEditing(true)
+        
+        // go back to the previous VC
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
