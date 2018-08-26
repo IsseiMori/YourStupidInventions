cbcc2bde380e512d28adb8e869923704bbadd7e6
IsseiMori <ganntyu1225issei@gmail.com>
2018-08-26 10:37:56 -0700

    add icons and add singInVC UI

---
+++ YourStupidInventions/SignIn.swift
@@ -0,0 +1,51 @@
+//
+//  SignIn.swift
+//  YourStupidInventions
+//
+//  Created by MoriIssei on 8/26/18.
+//  Copyright © 2018 IsseiMori. All rights reserved.
+//
+
+import UIKit
+
+class SignIn: UIViewController {
+    
+    // text fields
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    
+    // buttons
+    @IBOutlet weak var forgotBtn: UIButton!
+    @IBOutlet weak var signInBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // font of label
+        label.font = UIFont(name: "Pacifico", size: 25)
+        
+        // alignment
+        label.frame = CGRect(x: 10, y: 80, width: self.view.frame.size.width - 20, height: 50)
+        usernameTxt.frame = CGRect(x: 10, y: label.frame.origin.y + 70, width: self.view.frame.size.width - 20, height: 30)
+        passwordTxt.frame = CGRect(x: 10, y: usernameTxt.frame.origin.y + 40, width: self.view.frame.size.width - 20, height: 30)
+        forgotBtn.frame = CGRect(x: 10, y: passwordTxt.frame.origin.y + 30, width: self.view.frame.size.width - 20, height: 30)
+        
+        signInBtn.frame = CGRect(x: 20, y: forgotBtn.frame.origin.y + 40, width: self.view.frame.size.width / 4, height: 30)
+        signInBtn.layer.cornerRadius = signInBtn.frame.size.width / 20
+        
+        signUpBtn.frame = CGRect(x: self.view.frame.width - self.view.frame.size.width / 4 - 20, y: signInBtn.frame.origin.y, width: self.view.frame.size.width / 4, height: 30)
+        signUpBtn.layer.cornerRadius = signUpBtn.frame.size.width / 20
+        
+        // background
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        bg.image = UIImage(named: "bg.jpg")
+        bg.layer.zPosition = -1
+        self.view.addSubview(bg)
+        
+    }
+
+}
