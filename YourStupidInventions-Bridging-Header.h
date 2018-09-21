47e2b9f4ea561b8cb2c06034ba420f5f6ec8218b
IsseiMori <ganntyu1225issei@gmail.com>
2018-09-20 17:15:07 -0700

    fix header file issue

---
+++ YourStupidInventions-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  YourStupidInventions-Bridging-Header.h
+//  YourStupidInventions
+//
+//  Created by MoriIssei on 9/20/18.
+//  Copyright © 2018 IsseiMori. All rights reserved.
+//
+
+#ifndef YourStupidInventions_Bridging_Header_h
+#define YourStupidInventions_Bridging_Header_h
+
+#import <TwitterKit/TWTRKit.h>
+
+#endif /* YourStupidInventions_Bridging_Header_h */
